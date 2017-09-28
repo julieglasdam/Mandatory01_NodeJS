c89c6a98b07dd0d4392be7634aba13122b463124
julieglasdam <julieglasdam@hotmail.com>
2017-09-28 15:31:22 +0200

    changed port

--- index.js
+++ index.js
@@ -12,4 +12,6 @@ app.get('/', function(res, res) {
 	});
 })
 
-app.listen(7000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
